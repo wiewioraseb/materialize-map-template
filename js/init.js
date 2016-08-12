590a1f123e891f2b9f7f8563a8ffa96040293998
Tom Scholz <info@scholzworks.com>
2016-08-12 18:28:35 +0200

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.carousel').carousel();
+    $('ul.tabs').tabs();
+    $('.dropdown-button').dropdown({
+      belowOrigin: true,
+      hover: true
+    });
+    $('.collapsible').collapsible();
+    $('.button-collapse').sideNav({
+     menuWidth: 300
+   }
+ );
+});
